ffdd1bd28dfab984dd9566e2512569297c390bb4
kumadoara <kw2st68@hotmail.com>
2023-11-16 11:39:49 +0900

    Add files via upload

--- kadai_014/CarExec_Chapter14.java
+++ kadai_014/CarExec_Chapter14.java
@@ -4,7 +4,7 @@ public class CarExec_Chapter14 {
 
 	public static void main(String[] args) {
 		// 車クラスのインスタンスを作成する
-		Car_Chapter14 gear = new Car_Chapter14(3,30);
+		Car_Chapter14 gear = new Car_Chapter14(1,10);
 		Car_Chapter14 speed = new Car_Chapter14(3,30); 		
 		
 		// 車クラスのgearChangeメソッドを実行する
--- kadai_014/Car_Chapter14.java
+++ kadai_014/Car_Chapter14.java
@@ -3,17 +3,31 @@ package kadai_014;
 // 車クラス
 public class Car_Chapter14 {
 	// フィールド（内部データ）
-	private int gear = 1;	// 1速から5速のギアを表す
+	private int gear =  1;	// 1速から5速のギアを表す
 	private int speed = 10;	// ギアチェンジ後の速度を表す	
+	private int previousGear = 1;	// 切り替え前のギアの値
 	
 	// コンストラクタ（初期化処理）
 	public Car_Chapter14( int gear, int speed ) {
 		this.gear = gear;
-		this.speed = speed;		
+		this.speed = speed;			
+		
 	}	
+	
+	public int getGear() {
+	    return this.gear;
+	}
+
+	public int getSpeed() {
+	    return this.speed;
+	}
+
+	
+	
 	
 	// メソッド定義
 	public void gearChange( int afterGear ) {
+		this.previousGear = this.gear;
 		this.gear = afterGear;
 		// ギアの値により速度を変える
 		this.speed = switch(this.gear) {
@@ -26,10 +40,10 @@ public class Car_Chapter14 {
 		};
 		
 	}
-	
+		
 	public void run() {
-		System.out.println("ギア1から" + this.gear + "に切り替えました");
-		System.out.println("速度は時速" + this.speed + "kmです");
+		System.out.println("ギア" + this.previousGear  + "から" + this.getGear() + "に切り替えました");
+		System.out.println("速度は時速" +  this.getSpeed() + "kmです");
 	}
 	
 }
